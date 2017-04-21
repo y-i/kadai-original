c3c7804cc1c3f263a2f23a474d704393b032c234
y-i <y-i@users.noreply.github.com>
2017-04-21 06:18:31 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
+
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
   end
